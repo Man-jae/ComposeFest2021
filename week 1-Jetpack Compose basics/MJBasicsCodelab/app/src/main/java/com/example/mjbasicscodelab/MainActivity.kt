be01116d420e76dda7b79a2229e7981ac6252c16
Man-jae <alswo5052@gmail.com>
2021-11-09 16:38:09 +0900

    Styling and theming your app

--- week 1-Jetpack Compose basics/MJBasicsCodelab/app/src/main/java/com/example/mjbasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/MJBasicsCodelab/app/src/main/java/com/example/mjbasicscodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mjbasicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -101,7 +102,7 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.h4)
             }
             OutlinedButton(
                 onClick = { expanded.value = !expanded.value }
@@ -112,6 +113,12 @@ fun Greeting(name: String) {
     }
 }
 
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
