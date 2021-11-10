8b592977665f3d3ccde36998cbe54496982e8373
Man-jae <alswo5052@gmail.com>
2021-11-10 13:25:12 +0900

    week 2-1 Layouts in JetpackCompose 프로젝트 생성

---
+++ week 2-1-Layouts in JetpackCompose/MJCodelab/app/src/main/java/com/example/mjcodelab/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.mjcodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mjcodelab.ui.theme.MJCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MJCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MJCodelabTheme {
+        Greeting("Android")
+    }
+}
