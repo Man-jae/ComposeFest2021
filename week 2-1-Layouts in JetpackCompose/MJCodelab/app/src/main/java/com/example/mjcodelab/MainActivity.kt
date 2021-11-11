dbd154fd6f1b06fe80376b4592ae70e53be967a9
Man-jae <alswo5052@gmail.com>
2021-11-11 16:10:35 +0900

    Constraint Layout

--- week 2-1-Layouts in JetpackCompose/MJCodelab/app/src/main/java/com/example/mjcodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/MJCodelab/app/src/main/java/com/example/mjcodelab/MainActivity.kt
@@ -24,6 +24,10 @@ import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.*
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.atLeast
 import coil.compose.rememberImagePainter
 import com.example.mjcodelab.ui.theme.MJCodelabTheme
 import kotlinx.coroutines.launch
@@ -365,5 +369,133 @@ private class PaddingModifier(
                 placeable.place(start.roundToPx(), top.roundToPx())
             }
         }
+    }
+}
+
+/**
+ * Constraint Layout
+ */
+
+@Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+        val (button1, button2, text) = createRefs()
+
+        Button(
+            onClick = {},
+            modifier = Modifier.constrainAs(button1) {
+                top.linkTo(
+                    anchor = parent.top,
+                    margin = 16.dp
+                )
+            }
+        ) {
+            Text("Button 1")
+        }
+
+        Text(
+            text = "Text",
+            modifier = Modifier.constrainAs(text) {
+                top.linkTo(
+                    anchor = button1.bottom,
+                    margin = 16.dp
+                )
+                centerAround(button1.end)
+            }
+        )
+
+        val barrier = createEndBarrier(button1, text)
+
+        Button(
+            onClick = {},
+            modifier = Modifier.constrainAs(button2) {
+                top.linkTo(
+                    anchor = parent.top,
+                    margin = 16.dp
+                )
+                start.linkTo(barrier)
+            }
+        ) {
+            Text("Button 2")
+        }
+    }
+}
+
+@Composable
+fun LargeConstraintLayout() {
+    ConstraintLayout {
+        val text = createRef()
+        val guideline = createGuidelineFromStart(fraction = 0.5f)
+
+        Text(
+            text = "This is very very very very very very very very very very very very very very very long text!",
+            modifier = Modifier.constrainAs(text) {
+                linkTo(
+                    start = guideline,
+                    end = parent.end
+                )
+                width = Dimension.preferredWrapContent.atLeast(100.dp)
+            }
+        )
+    }
+}
+
+@Composable
+fun DecoupledConstraintLayout() {
+    BoxWithConstraints {
+        val constraints = if (maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp) // Portrait constraints
+        } else {
+            decoupledConstraints(margin = 32.dp) // Landscape constraints
+        }
+
+        ConstraintLayout(constraints) {
+            Button(
+                onClick = { /* Do something */ },
+                modifier = Modifier.layoutId("button")
+            ) {
+                Text("Button")
+            }
+
+            Text("Text", Modifier.layoutId("text"))
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin= margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ConstraintLayoutContentPreview() {
+    MJCodelabTheme {
+        ConstraintLayoutContent()
+    }
+}
+
+@Preview
+@Composable
+fun LargeConstraintLayoutPreview() {
+    MJCodelabTheme {
+        LargeConstraintLayout()
+    }
+}
+
+@Preview
+@Composable
+fun DecoupledConstraintLayoutPreview() {
+    MJCodelabTheme {
+        DecoupledConstraintLayout()
     }
 }
